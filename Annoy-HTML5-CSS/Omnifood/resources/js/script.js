32416e362dd426e718988f0dc895c26f73d5b38b
Alen Pontejos <alenpontejos@gmail.com>
2020-07-16 16:53:45 -0700

    added animations on scroll

--- Annoy-HTML5-CSS/Omnifood/resources/js/script.js
+++ Annoy-HTML5-CSS/Omnifood/resources/js/script.js
@@ -19,7 +19,27 @@ $('.js--scroll-to-start').click(function() {
   $('html, body').animate({scrollTop: $('.js--section-features').offset().top}, 500)
 })
 
-//scroll on buttons
+//animations on scroll
+$('.js--section-features').waypoint(function(direction) {
+  $('.features.fade-in').addClass('animate__animated animate__fadeIn');
+  $('.features.fade-in').removeClass('op-0');
+})
+
+$('.js--section-steps').waypoint(function(direction) {
+  $('.steps-box.fade-in').addClass('animate__animated animate__slideInUp');
+  $('.steps-box.fade-in').removeClass('op-0');
+}, {
+  offset: '200px'
+});
+
+$('.js--cities').waypoint(function(direction) {
+  $('.section-cities.fade-in').addClass('animate__animated animate__fadeIn');
+  $('.section-cities.fade-in').removeClass('op-0');
+})
+
+$('.js--section-plans').waypoint(function(direction) {
+  $('.plan-box.fade-in').addClass('animate__animated animate__pulse');
+})
 
 /*
 var waypoints = $('#handler-first').waypoint(function(direction) {
